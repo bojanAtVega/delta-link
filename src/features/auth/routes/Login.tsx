0a62554c56cb1fbddac6d51dbb35a3ef94f5b0a5
Bojan Kalanj <b.kalanj@vega.local>
2021-11-24 15:38:18 +0100

    Fix eslint warnings

--- src/features/auth/routes/Login.tsx
+++ src/features/auth/routes/Login.tsx
@@ -1,6 +1,5 @@
 import Layout from '../components/Layout';
 import LoginForm from '../components/LoginForm';
-import RegisterForm from '../components/RegisterForm';
 
 export const Login = () => {
   return (
