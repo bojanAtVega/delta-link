3c15ba6bc6f2949bd2ed2aa0003b88a3c87a2d63
Bojan Kalanj <b.kalanj@vega.local>
2021-11-25 16:24:22 +0100

    Test translations on Login page

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,9 +1,5 @@
-import { useTranslation } from 'react-i18next';
-
 export interface IRegisterFormProps {}
 
 export default function RegisterForm(props: IRegisterFormProps) {
-  const { t } = useTranslation();
-
-  return <div>{t('general.loggedOut')}</div>;
+  return <div>RegisterForm</div>;
 }
--- src/features/auth/routes/Login.tsx
+++ src/features/auth/routes/Login.tsx
@@ -1,9 +1,13 @@
 import Layout from '../components/Layout';
 import LoginForm from '../components/LoginForm';
+import { useTranslation } from 'react-i18next';
 
 export const Login = () => {
+  const { t } = useTranslation();
+
   return (
     <Layout>
+      <h1>{t('auth.login.header')}</h1>
       <LoginForm />
     </Layout>
   );
